e0a33349a13097e426baa45987a8034ea7fb6170
Krista <vanderhorst.20@wright.edu>
2024-07-30 22:00:28 -0400

    Fixed bucket and gold goblet anims

--- Globals/ItemDatabase.gd
+++ Globals/ItemDatabase.gd
@@ -20,11 +20,11 @@ var ITEMS = {
 	"pink_potion" : ItemDefClass.instantiate().with_data("pink_potion", "Pink Potion", "A mystical pink potion", "anim_pink_potion", POTION_SOUND, "res://Assets/Items/Potions/Small Vial - PINK.png"),
 	"black_potion" : ItemDefClass.instantiate().with_data("black_potion", "Black Potion", "A mystical black potion", "anim_black_potion", POTION_SOUND, "res://Assets/Items/Potions/Classic Jar - BLACK.png"),
 	"green_potion" : ItemDefClass.instantiate().with_data("green_potion", "Green Potion", "A mystical green potion", "anim_green_potion", POTION_SOUND, "res://Assets/Items/Potions/Large Jar - TURQUOISE.png"),
-	"gold_goblet" : ItemDefClass.instantiate().with_data("gold_goblet", "Gold", "Looks shiny...", "gold", POTION_SOUND, "res://Assets/Items/Potions/GoldenChalice.png"),
-	"water_bucket" : ItemDefClass.instantiate().with_data("water_bucket", "Bucket of Water", "Looks wet...", "water_bucket", POTION_SOUND, "res://Assets/Items/Potions/WaterBucket.png"),
-	"aqua_regia" : ItemDefClass.instantiate().with_data("aqua_regia", "Aqua Regia", "A mystical potion that can melt any metal", "aqua_regia", POTION_SOUND, "res://Assets/Items/Acid.png"),
-	"journal": ItemDefClass.instantiate().with_data("journal", "Old Journal", "A journal with alchemical recipes...this could be useful", "journal", "res://Assets/Sounds/JournalPickup.ogg", "res://Assets/Items/Journal.png"),
-	"journalpage_aquaregia": ItemDefClass.instantiate().with_data("journalpage_aquaregia", "Torn Journal Page", "A ripped journal page with some kind of ... alchemical symbols?", "aqua_regia_recipe", "res://Assets/Sounds/JournalPageTurn.wav", "res://Assets/Items/Clues/AquaRegiaHintImage.png"),
+	"gold_goblet" : ItemDefClass.instantiate().with_data("gold_goblet", "Gold", "Looks shiny...", "anim_gold_goblet", POTION_SOUND, "res://Assets/Items/Potions/GoldenChalice.png"),
+	"water_bucket" : ItemDefClass.instantiate().with_data("water_bucket", "Bucket of Water", "Looks wet...", "anim_water_bucket", POTION_SOUND, "res://Assets/Items/Potions/WaterBucket.png"),
+	"aqua_regia" : ItemDefClass.instantiate().with_data("aqua_regia", "Aqua Regia", "A mystical potion that can melt any metal", "anim_aqua_regia", POTION_SOUND, "res://Assets/Items/Acid.png"),
+	"journal": ItemDefClass.instantiate().with_data("journal", "Old Journal", "A journal with alchemical recipes...this could be useful", "anim_journal", "res://Assets/Sounds/JournalPickup.ogg", "res://Assets/Items/Journal.png"),
+	"journalpage_aquaregia": ItemDefClass.instantiate().with_data("journalpage_aquaregia", "Torn Journal Page", "A ripped journal page with some kind of ... alchemical symbols?", "anim_aqua_regia_recipe", "res://Assets/Sounds/JournalPageTurn.wav", "res://Assets/Items/Clues/AquaRegiaHintImage.png"),
 	"light_potion" : ItemDefClass.instantiate().with_data("light_potion", "Light Potion", "A mystical light potion", "anim_light_potion", POTION_SOUND, "res://Assets/Items/Potions/Small Vial - PINK.png", true),
 	# Put more items here!
 	}
